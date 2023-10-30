2a2992dff6001f6839115b29fa78e06df470575a
Timur Babkin <timurbabkin.pshh@gmail.com>
2023-10-30 23:08:20 +0100

    fixed router

--- packages/frontend/src/router.tsx
+++ packages/frontend/src/router.tsx
@@ -1,9 +1,15 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import { MainPage } from "./pages";
+
+const BASE_URL = "/photos-of-no-w-here/";
 
 export const router = createBrowserRouter([
   {
     path: "/",
+    element: <Navigate to={BASE_URL} />,
+  },
+  {
+    path: BASE_URL,
     element: <MainPage />,
   },
 ]);
