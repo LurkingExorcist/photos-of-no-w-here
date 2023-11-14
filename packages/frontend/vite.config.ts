4e25a908db20e5659027b938b188fe79ee7a5855
Timur Babkin <timur.babkin@check24.de>
2023-11-14 21:55:35 +0100

    set frontend ports

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/photos-of-no-w-here/',
+  server: {
+    port: 3030,
+  },
+  preview: {
+    port: 8080,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
